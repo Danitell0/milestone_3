490b621055cd43e3442c89f7a365761c3dfe4bf1
Danitell0 <danielfernandesdesigns@gmail.com>
2026-07-22 21:23:24 +0200

    updated exam questions

--- exam_rank03/py_cryptic_sorter.py
+++ exam_rank03/py_cryptic_sorter.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
+def count_vowels(s: str) -> int:
+    vowels = "aeiou"
+    counter = 0
+
+    for char in s:
+        if char in vowels:
+            counter += 1
+    return counter
+
 def check_swap(s1: str, s2: str) -> bool:
+    s1_l = s1.lower()
+    s2_l = s2.lower()
+
     if len(s1) > len(s2):
         return True
+    elif len(s1) == len(s2):
+        if s1_l > s2_l:
+            return True
+        elif count_vowels(s1_l) > count_vowels(s2_l):
+            return True
     return False
 
 def cryptic_sorter(strings: list[str]) -> list[str]:
@@ -16,7 +33,6 @@ def cryptic_sorter(strings: list[str]) -> list[str]:
                 swapped = True
         if not swapped:
             break
-
     return strings
 
 if __name__ == "__main__":
@@ -25,3 +41,6 @@ if __name__ == "__main__":
     list_3 = ["hello","world","hi","test"]
 
     print(cryptic_sorter(list_1))
+    print(cryptic_sorter(list_2))
+    print(cryptic_sorter(list_3))
+    
