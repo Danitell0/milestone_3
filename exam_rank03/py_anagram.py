93d825e304d81c74583b363ff1bd051118dabb76
Danitell0 <danielfernandesdesigns@gmail.com>
2026-07-24 13:31:37 +0200

    anagram, shadow_merge added

---
+++ exam_rank03/py_anagram.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def anagram(s1: str, s2: str) -> bool:
+    s1 = s1.replace(" ", "")
+    s2 = s2.replace(" ", "")
+    if len(s1) != len(s2):
+        return False
+    for char in s1.lower():
+        if char not in s2.lower():
+            return False
+    return True
+
+print(anagram("listen", "silent"))
+print(anagram("Dormitory", "Dirty Room"))
+print(anagram("hello", "world"))
+print(anagram("", ""))
+print(anagram("abc", "abcc"))
---
+++ exam_rank03/py_shadow_merge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def shadow_merge(list1: list[int], list2: list[int]) -> list[int]:
+    list1.extend(list2)
+    return sorted(list1)
+
+print(shadow_merge([1,3,5], [2,4,6]))
+print(shadow_merge([1,2,3], [4,5,6]))
+print(shadow_merge([1], [2,3,4]))
+print(shadow_merge([], [1,2,3]))
+print(shadow_merge([1,1,2], [1,3,3]))
