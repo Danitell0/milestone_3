d766a37ed4c78516a1a27ab3734ade7b3d9e4afe
Danitell0 <danielfernandesdesigns@gmail.com>
2026-07-25 12:24:52 +0200

    finally fixed cryptic_sorter

--- exam_rank03/py_cryptic_sorter.py
+++ exam_rank03/py_cryptic_sorter.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python3
 
-def count_vowels(s: str) -> int:
-    vowels = "aeiou"
-    counter = 0
+def swap_check(a: str, b: str) -> bool:
+    if len(a) != len(b):
+        return len(a) < len(b)
+    for ch1, ch2 in zip(a, b):
+        if ch1.lower() != ch2.lower():
+            return ch1.lower() < ch2.lower()
 
-    for char in s:
-        if char in vowels:
-            counter += 1
-    return counter
+    vowels = "aeiouAEIOU"
 
-def check_swap(s1: str, s2: str) -> bool:
-    s1_l = s1.lower()
-    s2_l = s2.lower()
-
-    if len(s1) > len(s2):
-        return True
-    elif len(s1) == len(s2):
-        if s1_l > s2_l:
-            return True
-        elif count_vowels(s1_l) > count_vowels(s2_l):
-            return True
-    return False
+    count_a = sum(ch in vowels for ch in a)
+    count_b = sum(ch in vowels for ch in b)
+    if count_a != count_b:
+        return count_a < count_b
 
 def cryptic_sorter(strings: list[str]) -> list[str]:
-    limit = len(strings)
+    for i in range(1, len(strings)):
+        current = strings[i]
+        j = i - 1
 
-    for i in range(limit):
-        swapped = False
-        for j in range(0, limit - i - 1):
-            if check_swap(strings[j], strings[j + 1]):
-                strings[j], strings[j + 1] = strings[j + 1], strings[j]
-                swapped = True
-        if not swapped:
-            break
+        while j >= 0 and swap_check(current, strings[j]):
+            strings[j + 1] = strings[j]
+            j -= 1
+
+        strings[j + 1] = current
     return strings
 
 if __name__ == "__main__":
@@ -43,4 +34,6 @@ if __name__ == "__main__":
     print(cryptic_sorter(list_1))
     print(cryptic_sorter(list_2))
     print(cryptic_sorter(list_3))
-    
+    print(cryptic_sorter([]))
+    print(cryptic_sorter([""]))
+
