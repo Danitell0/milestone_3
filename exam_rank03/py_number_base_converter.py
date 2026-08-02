afcffea707d17a2c32b31ccfedc2df65d2410aac
Danitell0 <danielfernandesdesigns@gmail.com>
2026-08-02 18:18:53 +0200

    shorter version using int(nb, from_base)

--- exam_rank03/py_number_base_converter.py
+++ exam_rank03/py_number_base_converter.py
@@ -6,30 +6,20 @@ def number_base_converter(number: str, from_base: int, to_base: int) -> str:
     if not (2 <= from_base <= 36 and 2 <= to_base <= 36):
         return "ERROR"
 
-    number = number.upper()
-
-    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    decimal = 0
-    for char in number:
-        if char not in digits:
-            return "ERROR"
-
-        value = digits.index(char)
-
-        if value >= from_base:
-            return "ERROR"
-
-        decimal = decimal * from_base + value
-
+    try:
+        decimal = int(number, from_base)
+    except:
+        return "ERROR"
     if decimal == 0:
         return "0"
-
+    
     result = []
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     while decimal > 0:
         decimal, remainder = divmod(decimal, to_base)
         result.append(digits[remainder])
-
     return "".join(reversed(result))
+
 
 print(number_base_converter("101010", 2, 10))
 print(number_base_converter("FF", 16, 10))
