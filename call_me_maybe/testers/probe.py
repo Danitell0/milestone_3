007e699aa04de14fc3167a8575bdc0ae95e414b6
Daniel Filipe Moreira Fernandes <danmorei@f1r4s18.codam.nl>
2026-08-04 19:46:46 +0200

    testing with LLM and BPE

--- call_me_maybe/testers/probe.py
+++ call_me_maybe/testers/probe.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
+import time
 from llm_sdk import Small_LLM_Model
 
 potato = Small_LLM_Model()
 
 reverse_string = potato.encode("fn_reverse_string")[0].tolist()
 for i in reverse_string:
-    print(potato.decode(i))
+    print(i, repr(potato.decode(i)))
 
 add_numbers = potato.encode("fn_add_numbers")[0].tolist()
 for i in add_numbers:
-    print(potato.decode(i))
+    print(i, repr(potato.decode(i)))
 
 greet = potato.encode("fn_greet")[0].tolist()
 for i in greet:
-    print(potato.decode(i))
+    print(i, repr(potato.decode(i)))
 
 square_root = potato.encode("fn_get_square_root")[0].tolist()
 for i in square_root:
-    print(potato.decode(i))
-
+    print(i, repr(potato.decode(i)))
 
 regex = potato.encode("fn_substitute_string_with_regex")[0].tolist()
 for i in regex:
-    print(potato.decode(i))
+    print(i, repr(potato.decode(i)))
 
+t = time.perf_counter()
+logits = potato.get_logits_from_input_ids(reverse_string)
+print(len(logits), (time.perf_counter() - t) * 1000, "ms")
